12dbb59b92af8ec962c618baf01dff4f5f651cab
Ian McFarland <imf@Beryllium.local>
2013-02-05 18:59:17 -0800

    LED now responds to morse key state.

--- morse_in.ino
+++ morse_in.ino
@@ -3,12 +3,19 @@ static int SOUND_OUT = 9;
 static int LED = 13;
 
 void setup() {
-//  Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(MORSE_KEY, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   int keyState = digitalRead(MORSE_KEY);
+  if (keyState) {
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
   Serial.println(keyState);
-  delay(100);
+  delay(1);
 }
