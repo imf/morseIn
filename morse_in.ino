33d294bbe817ccd70fc8b4020a3a42c52ac7d154
Ian McFarland <imf@Beryllium.local>
2013-02-05 19:02:15 -0800

    Now with sound... Untested sound, but sound.

--- morse_in.ino
+++ morse_in.ino
@@ -2,8 +2,10 @@ static int MORSE_KEY = 2;
 static int SOUND_OUT = 9;
 static int LED = 13;
 
+static int FREQUENCY = 1000;
+
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   pinMode(MORSE_KEY, INPUT);
   pinMode(LED, OUTPUT);
 }
@@ -12,10 +14,14 @@ void loop() {
   int keyState = digitalRead(MORSE_KEY);
   if (keyState) {
     digitalWrite(LED, HIGH);
+    digitalWrite(SOUND_OUT, HIGH);
+    delayMicroseconds(FREQUENCY / 2);
+    digitalWrite(SOUND_OUT, LOW);
+    delayMicroseconds(FREQUENCY / 2);
   }
   else {
     digitalWrite(LED, LOW);
+    delay(1);
   }
-  Serial.println(keyState);
-  delay(1);
+//  Serial.println(keyState);
 }
