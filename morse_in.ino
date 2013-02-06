65b3bc13854e57370f6813ac20fb2d8bda3cc0d9
Ian McFarland <imf@Beryllium.local>
2013-02-05 19:11:28 -0800

    It works! Works like a charm. I learned how to do stuff today.

--- morse_in.ino
+++ morse_in.ino
@@ -1,3 +1,14 @@
+/*
+  Morse Key LED and Tone Emitter
+  
+  Wiring:
+    Connect morse key line in to 5V out.
+    Connect morse key tel out to digital port 2 (MORSE_KEY)
+    Connect 10KΩ resistor between digital port 2 and ground
+    Connect speaker between ground and digital port 9 (SOUND_OUT)
+
+*/
+
 static int MORSE_KEY = 2;
 static int SOUND_OUT = 9;
 static int LED = 13;
@@ -5,7 +16,6 @@ static int LED = 13;
 static int FREQUENCY = 1000;
 
 void setup() {
-//  Serial.begin(9600);
   pinMode(MORSE_KEY, INPUT);
   pinMode(LED, OUTPUT);
 }
@@ -23,5 +33,4 @@ void loop() {
     digitalWrite(LED, LOW);
     delay(1);
   }
-//  Serial.println(keyState);
 }
